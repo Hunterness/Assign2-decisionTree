db200b4e39fc6c1d044e733ad934991b8daa5cbc
Therese Magnusson <dat13tma@student.lu.se>
2017-02-10 11:35:25 +0100

    Finding lines if input

--- decisionTree.py
+++ decisionTree.py
@@ -1,8 +1,40 @@
+import regex as re
+
+def reader(filename):
+    file = open(filename)
+    input = file.read()
+    attributes = re.finditer('(.*\n)', input)
+    for line in attributes:
+        word = line.group().replace('\n', '')
+        pos = word.find('%')
+        if(pos != -1):
+            word = word[0:pos]
+        if(word != ''):
+            print(word)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     column_names = ['Alternative', 'Bar', 'Fri/Sat', 'Hungry', 'Patrons',
     'Price', 'Raining', 'Reservation', 'Type', 'WaitEstimate']
     filename = 'input.arff'
-    file = open(filename)
-    input = file.readlines()
-    print(input[0])
+    reader(filename)
