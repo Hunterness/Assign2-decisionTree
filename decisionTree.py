71cffaeadf89e73738945c870e9e3e3b8f21ff01
Therese Magnusson <dat13tma@student.lu.se>
2017-02-14 10:42:06 +0100

    merge to one file

--- decisionTree.py
+++ decisionTree.py
@@ -1,5 +1,6 @@
 import regex as re
 
+"""Reader. reads in ARFF-file"""
 def reader(filename):
     attributes = {}
     file = open(filename)
@@ -42,8 +43,33 @@ def reader(filename):
     return attributes,data
 
 
+"""Algorithm for Decision Tree"""
+def decision_tree_algorithm(examples, attributes,parent_examples):
+	"""if examples.empty
+			return plurality_value(parent_examples)
+		else if all examples have samma classification
+			return
+		else if attributes.empty
+			return plurality_value(examples)
+		else
+			A = importance
+			tree = new decision tree with root A
+			for each v_k of A
+				exs = [e of examples ^ e.A = v_k]
+				subtree = decision_tree_algorithm(exs, attributes-A,examples)
+				add branch to tree with label (A=v_k) and subtree "subtree"
+		return tree
+	"""
+
+
+def importance(attributes):
+
+def plurality_value(examples):
+
+
 if __name__ == '__main__':
     #column_names = ['Alternative', 'Bar', 'Fri/Sat', 'Hungry', 'Patrons',
     #'Price', 'Raining', 'Reservation', 'Type', 'WaitEstimate']
     filename = 'input_withRes.arff'
     attribute, data = reader(filename)
+    decision_tree_algorithm(data, attribute, data)
