01f5dba562799f2af825f3f538988842e50ca343
Therese Magnusson <dat13tma@student.lu.se>
2017-02-14 10:31:50 +0100

    inläsning klar?

--- decisionTree.py
+++ decisionTree.py
@@ -1,10 +1,13 @@
 import regex as re
 
 def reader(filename):
+    attributes = {}
     file = open(filename)
     input = file.read().lower()
-    attributes = re.finditer('(.*\n)', input)
-    for line in attributes:
+    inputLines = re.finditer('(.*\n)', input)
+    counterAttr = 0
+    counterData = 0
+    for line in inputLines:
         word = line.group().replace('\n', '')
         pos = word.find('%')
         if(pos != -1):
@@ -15,10 +18,37 @@ def reader(filename):
                 pos = word.find(' {')
                 if(pos != -1):
                     attr = word[11:pos]
-                    types = word[pos+2:len(word)-1]
-                print("attribut: ", attr, "types: ", types)
+                    types = word[pos+2:len(word)-1]+","
+                pos = types.find(',')
+                attributes[counterAttr] = [attr]
+                while(pos != -1):
+                    attributes[counterAttr].append(types[0:pos])
+                    types = types[pos+1:len(types)]
+                    pos = types.find(',')
+                counterAttr = counterAttr+1
+
             else:
-                print("data: ", word)
+                word = word+","
+                data = {}
+                for i in range(0,len(attributes.keys())):
+                    pos = word.find(',')
+                    if(pos != -1):
+                        a = word[0:pos]
+                        try:
+                            data[counterData].append((attributes[i][0],a))
+                        except:
+                            data[counterData] = [(attributes[i][0],a)]
+
+
+                        word = word[pos+1:len(word)]
+                counterData = counterData+1
+
+
+
+
+
+
+    return attributes,data
 
 
 
@@ -27,6 +57,30 @@ def reader(filename):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Algorithm
 
 
 
@@ -41,7 +95,7 @@ def reader(filename):
 
 
 if __name__ == '__main__':
-    column_names = ['Alternative', 'Bar', 'Fri/Sat', 'Hungry', 'Patrons',
-    'Price', 'Raining', 'Reservation', 'Type', 'WaitEstimate']
-    filename = 'input.arff'
-    reader(filename)
+    #column_names = ['Alternative', 'Bar', 'Fri/Sat', 'Hungry', 'Patrons',
+    #'Price', 'Raining', 'Reservation', 'Type', 'WaitEstimate']
+    filename = 'input_withRes.arff'
+    attribute, data = reader(filename)
