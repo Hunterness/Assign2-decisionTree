ef6eb541f36915ef67cc4329a98b9ee4ba58db58
Therese Magnusson <dat13tma@student.lu.se>
2017-02-17 18:05:32 +0100

    Uppsnyggning av kod, tog bort kommentarer

--- decisionTree.py
+++ decisionTree.py
@@ -2,7 +2,9 @@ import regex as re
 from collections import OrderedDict
 from math import log2
 
-"""Reader. reads in ARFF-file"""
+"""Reader. reads in ARFF-file
+    Can't handle '?' in data or
+    any valutypes not within {} for the attributes"""
 def reader(filename):
     attributes = {}
     data = {}
@@ -94,12 +96,7 @@ def nbrTotal(examples, attributes, attr,types):
             if e[-1][1] == types[k]:
                 nbr[k] = nbr[k] + 1
     return nbr
-"""
-def B(q,types):
-    if q == 0 or q == 1:
-        return 0
-    return -(q*log2(q)+(1-q)*log2(1-q))
-"""
+
 def H(nbrTypes, sum):
     if sum == 0:
         return 0
@@ -133,37 +130,6 @@ def importance(attributes,examples,types):
                 max_gain = gain
     return max_attr
 
-    """
-    max_attr = ''
-    max_gain = 0
-    for a in attributes:
-        if a != 'willwait':
-            remainder = 0
-            nbrT = nbrTotal(examples,attributes,a,types)
-            pT = nbrT[0]
-            nT = nbrT[1]
-            sumT = 0
-            for k in range(len(types)):
-                sumT = sumT + nbrT[k]
-            for v in attributes[a][1:len(attributes[a])]:
-                nbrV = nbrPerVal(examples,attributes,a,v,types)
-                #print(a,v,(pV+nV),(pT+nT))
-                pV = nbrV[0]
-                nV = nbrV[1]
-                sumV = 0
-                for k in range(len(types)):
-                    sumV = sumV + nbrV[k]
-                if sumV != 0:
-                    q = pV/sumV
-                    remainder = remainder + B(q,types)*sumV/sumT
-
-            gain = B(pT/sumT,types)-remainder
-            if gain > max_gain:
-                max_attr = a
-                max_gain = gain
-
-    return max_attr
-"""
 
 def plurality_value(examples,types):
     nbr = []
@@ -180,85 +146,39 @@ def plurality_value(examples,types):
             ans = types[k]
             max = nbr[k]
     return ans
-    """if nbrYes == nbrNo:
-        return examples[0][-1][1]
-    elif nbrYes > nbrNo:
-        return 'yes'
-    else:
-        return 'no'
-    """
 
 
 def decision_tree_algorithm(examples, attributes,parent_examples,turn):
-    """if examples.empty
-			return plurality_value(parent_examples)
-		else if all examples have samma classification
-			return
-		else if attributes.empty
-			return plurality_value(examples)
-		else
-			A = importance
-			tree = new decision tree with root A
-			for each v_k of A
-				exs = [e of examples ^ e.A = v_k]
-				subtree = decision_tree_algorithm(exs, attributes-A,examples)
-				add branch to tree with label (A=v_k) and subtree "subtree"
-		return tree"""
     types = attributes['willwait'][1:len(attributes['willwait'])]
     if not examples:
         #no more data
-        #print("no data")
         treePrint.append((": " + plurality_value(parent_examples,types),0,True))
-        #return ": " + plurality_value(parent_examples,types) + "\n", True
     elif same_class(examples,types):
-        #print("same class")
         #alla kvarvarande exempel har samma resultat
         treePrint.append((": " + examples[0][-1][1],0,True))
-        #return ": " + examples[0][-1][1] + "\n", True #returnerar yes/no
     elif not attributes:
         #no more attributes
-        #print("no attr")
         treePrint.append((": " + plurality_value(examples,types),0,True))
-        #return ": " + plurality_value(examples,types) + "\n", True
     else:
         a = importance(attributes, examples,types)
-        #tree = ""
         i = attributes[a][0]
         values = attributes[a][1:len(attributes[a])]
         del attributes[a]
         for v in values:
-            #tree = tree + a + " = " + v
             treePrint.append((a + " = " + v,turn,False))
             exs = []
             for j in range(len(examples)):
                 res = examples[j][i][1]
                 if res == v:
                     exs.append(examples[j])
-            #subtree, end =
             decision_tree_algorithm(exs,attributes,examples,turn+1)
-            """if end:
-                tree = tree + subtree
-            else:
-                tree = tree + "\n"
-                for k in range(turn+1):
-                    tree = tree + "\t"
-                tree = tree + subtree
-                for k in range(turn+1):
-                    tree = tree + "\t"""
-
-        #return tree, False
-
-
-
 
 
 if __name__ == '__main__':
     filename = 'input_2res.arff'
     attributes, data = reader(filename)
-    #tree, end =
     decision_tree_algorithm(data, attributes, data,0)
     print("Restaurant 2:")
-    #print(tree)
     printout = ""
     for i in range(len(treePrint)-1):
         if not treePrint[i][2] and treePrint[i+1][2]:
@@ -272,12 +192,11 @@ if __name__ == '__main__':
             printout = printout + treePrint[i][0] + "\n"
 
     print(printout)
+
+
     treePrint = []
-
-
     filename = 'input_3res.arff'
     attributes, data = reader(filename)
-    #tree, end =
     decision_tree_algorithm(data, attributes, data,0)
     print("\nRestaurant 3:")
     printout = ""
